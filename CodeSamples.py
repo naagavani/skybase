8285cc83ab38598828e3bd99c2a0f91023852d19
sai_windows <sai_windows@example.com>
2019-05-06 23:42:32 -0500

    Added Calculator Class

--- CodeSamples.py
+++ CodeSamples.py
@@ -2,3 +2,16 @@
 a=10
 b=20
 print("The value of the sum of variables is: {}".format(a+b))
+
+class Calculator() :
+    def __init__(self):
+        self.var1 = 10
+        self.var2 = 20
+
+    def __str__(self):
+        return f"The sum of variables is: {self.var1+self.var2}"
+
+c = Calculator()
+print(c)
+
+
